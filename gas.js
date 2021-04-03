942b93ff249fe3c641059de38a4fd2da20c1582e
Yuzunoha <studysteadystandby@gmail.com>
2021-04-03 12:26:00 +0900

    Update gas.js
    3行目を左から9セル分

--- gas.js
+++ gas.js
@@ -7,6 +7,7 @@ const myFunction = () => {
   var myRange = sheet.getDataRange().getValues();
   p(myRange.length);
 
-  var range = sheet.getRange(1, 4);
+  // getRange(row, column, numRows, numColumns)
+  var range = sheet.getRange(3, 1, 1, 9); // 3行目を左から9セル分
   range.setBackground('pink'); // 背景色をピンクに
 };
